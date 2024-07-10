ebf0ada495005e997cb767d1c215f86734dcb4c7
RakshithaGuthikonda <rakshithaguthikonda@gmail.com>
2024-07-10 15:12:28 +0530

    Update cluster.tf

--- Terraform-manifest/cluster.tf
+++ Terraform-manifest/cluster.tf
@@ -2,7 +2,7 @@ resource "azurerm_kubernetes_cluster" "aks1" {
   name                = "rakshitha-aks-tf-az"
   location            = azurerm_resource_group.rg2.location
   resource_group_name = azurerm_resource_group.rg2.name
-  dns_prefix          = "${azurerm_resource_group.rg2.name}-dns"
+  dns_prefix          = "rakshithadns"
   kubernetes_version = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.rg2.name}-nrg-bhanu"
 
